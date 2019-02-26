dcd86c16fcad764360fce1322a5deffcc2c88f13
Michael Wenz <wenzm9@gmail.com>
2019-02-26 19:41:01 +0800

    finished

--- week_02/06_tuples/01_make_tuples.py
+++ week_02/06_tuples/01_make_tuples.py
@@ -7,46 +7,34 @@ Write a script that takes in a list of numbers and:
 
 Notes:
 
-If the user enters an odd numbered list, add the last item
+If the user enters an odd numbered items in the list, add the last item
 to a tuple with the number 0.
 
 '''
 
-#_list = [2, 3, 4, 2, 1, 4, 21, 4, 566, 5, 9]
-
 try:
-    _list = (input('Enter numbers with spaces : ')).split()
+    list_ = (input('Enter numbers with spaces : ')).split()
 
     f_list = []
 
-    if len(_list) % 2 == 0:
-        #print('length is even', len(_list))
-        for item in _list:
-            f_list.append(float(item)) # Convert list to float
-    else: #add a zero to the list to make it even
-        _list.append(0)
-        for item in _list:
-            f_list.append(float(item)) # Convert list to float
+    for item in list_:
+        f_list.append(float(item))  # Convert list to float
 
 except ValueError:
     print('that\'s not a number restart the script')
 
 f_list.sort()
 
-
+if len(f_list) % 2 != 0:
+    f_list.append(float(0))
 
 #create pairs
 #print('Length ',len(f_list))
 
 tuple_pair_list = []
-count = 0
+
 for i in range(0, len(f_list), 2):
-    #print(i)
-    count += i
     tuple_pair = (f_list[i], f_list[i+1])
-    #print('index', i, i +1)
-    #print(i,'count ', count)
-    #print(tuple_pair)
     tuple_pair_list.append(tuple_pair)
 
 print('Tuple Pair List', tuple_pair_list)
