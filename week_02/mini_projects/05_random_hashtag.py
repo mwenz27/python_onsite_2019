9510cb7dcce7c7b126e7afb3a6eb26440edb5d5c
Michael Wenz <wenzm9@gmail.com>
2019-03-01 17:11:32 +0800

    finished

--- week_02/mini_projects/05_random_hashtag.py
+++ week_02/mini_projects/05_random_hashtag.py
@@ -9,27 +9,17 @@ Tip: In UNIX systems you can access a dictionary file at this path:
         /usr/share/dict/words
 
 '''
-#read the file
+
+import random
 
 file = '/usr/share/dict/words'
+
 with open(file, encoding='utf-8') as words:
     contents = words.read()
+    words = contents.split() #create a list for words
+
+for i in range(10):
+    number = random.randint(1, len(words))
+    print(f'#{words[number].capitalize()}', end=" ")
 
 
-with open(file, encoding='utf-8') as words:
-    contents = words.read() #also can be done in a iteration using the r.strip function
-    #for word in words.readlines():
-    #word = word.rstrip()
-    #append(word)
-    words = contents.split() # creates a list
-    words.sort(key=len) #sorts the list by length
-    print(words[:10]) # test line
-    min_words_list = []
-    max_words_list = []
-#create a list for words
-
-#find the length of the words
-
-#use the random function and select a word for a hastag
-
-#print hash tag
