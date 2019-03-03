25a9c4cf0e29dbcb3ba6a3961dc27fcfad1cb7cc
Michael Wenz <wenzm9@gmail.com>
2019-03-03 17:29:40 +0800

    finished

--- week_02/10_classes_objects_methods/02_points.py
+++ week_02/10_classes_objects_methods/02_points.py
@@ -7,16 +7,50 @@ build out the Point class example.
 
 
 class Point:
-    def __init__(self, x, y):
+    def __init__(self, x = None, y = None):
         self.x = x
         self.y = y
 
+    def distance_between_points(self):
+        a = self.x
+        b = self.y
+        c = (a**2 + b**2)**0.5
+        return f'The distance between points {a}, {b} is {c}'
+
+
 class Rectangle:
-    def __init__(self, width, height, corner):
+    def __init__(self, width=None, height=None , corner=Point()):
         self.width = width
         self.height = height
-        self.corner = corner
+        self.corner = corner # corner is a Point object that specifies the lower-left corner
 
-    def find_center(self):
-        p = Point()
-        p.x =
+    def find_centre(self):
+        x = self.corner.x + self.width/2
+        y = self.corner.y + self.height/2
+        return f'centre point is {x} and {y}'
+
+
+def find_centre2(width, hieght, corner):
+    x1 = width/2 + corner.x
+    y1 = hieght/2 + corner.y
+    return f'centre point is {x1} and {y1}'
+
+
+
+mark = Point(3, 4)
+print(mark.distance_between_points())
+
+box = Rectangle()
+box.width = 100
+box.height = 200
+box.corner = Point()
+box.corner.x = 0
+box.corner.y = 0
+
+
+print(box.find_centre()) # finding the centre using a method in my class already created
+
+
+print(find_centre2(100, 200, Point(0,0)) )
+
+
