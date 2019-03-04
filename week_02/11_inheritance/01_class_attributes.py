41cb8c786493b07c40117e95986a13c3e6010c3d
Michael Wenz <wenzm9@gmail.com>
2019-03-04 08:59:35 +0800

    finished

--- week_02/11_inheritance/01_class_attributes.py
+++ week_02/11_inheritance/01_class_attributes.py
@@ -10,19 +10,33 @@ Flush out the classes below with the following:
 '''
 
 class Class1:
-
+    def __init__(self):
+        self.x = 10
     # define an __init__() method that sets an attribute x
-    pass
 
-class Class2:
 
+class Class2(Class1): # inheritance from class one
+    def __init__(self):
+        self.y = 20
+        super().__init__()
     # define an __init__() method that sets an attribute y and calls the __init__() method of its parent
-    pass
 
-class Class3:
+
+class Class3(Class2):
+    def __init__(self):
+        self.z = 30
+        super().__init__()
 
     # define an __init__() method that sets an attribute z and calls the __init__() method of its parent
-    pass
 
 
 # create an object of each class and print each of its attributes
+object_1 = Class1()
+print(object_1.x)
+
+
+object_2 = Class2()
+print(object_2.x, object_2.y)
+
+object_3 = Class3()
+print(object_3.x, object_3.y, object_3.z)
