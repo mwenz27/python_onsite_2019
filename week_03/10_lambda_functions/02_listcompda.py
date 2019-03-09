4321910a64d2a741512a99b962aa356c28ddb0d1
Michael Wenz <wenzm9@gmail.com>
2019-03-10 07:38:17 +0800

    finished

--- week_03/10_lambda_functions/02_listcompda.py
+++ week_03/10_lambda_functions/02_listcompda.py
@@ -5,12 +5,6 @@ Re-write the following listcomp as a lambda expression.
 
 names = ['Anne', 'Amy', 'Bob', 'David', 'Carrie', 'Barbara', 'Zach']
 
-#print([x.startswith('D') for x in names])
-
-# d_in_names = print(lamba name : for name in names if "D" in name)
-
-# listcomp = [name for name in fish_tuple if 'fish' in name]
-
 f = lambda x: x.startswith('D')
 
 for name in names:
--- week_03/10_lambda_functions/03_lamprint.py
+++ week_03/10_lambda_functions/03_lamprint.py
@@ -4,3 +4,8 @@ Create a lambda expression that takes no input and prints "get serious!"
 What does it return?
 
 '''
+
+
+ff = lambda : print('get serious!')
+
+ff()
