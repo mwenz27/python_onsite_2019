c6f578eccfeaa6216e9e2fdf0a482a4e1741c863
Michael Wenz <wenzm9@gmail.com>
2019-03-20 14:29:02 +0800

    update wednesday

---
+++ week_05/more_APIs/slack_folder/slack_post_api.py
@@ -0,0 +1,12 @@
+from slackclient import SlackClient
+
+from config import config
+from pprint import pprint
+
+slack_args = {
+    'botland' : 'CH'
+
+}
+xoxp-561901429298-571745030705-581343423061-c617ac8a38648d390aa76dbe4808e81e
+sc = SlackClient(config[password])
+
