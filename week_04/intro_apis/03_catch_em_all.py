7fed90851632e73808ba29f1eaeaf792246564e0
Michael Wenz <wenzm9@gmail.com>
2019-03-25 15:06:12 +0800

    updated ignor file

--- week_04/intro_apis/03_catch_em_all.py
+++ week_04/intro_apis/03_catch_em_all.py
@@ -13,6 +13,7 @@ BONUS: Using your script, create a folder and download the main 'front_default'.
 '''
 
 import requests
+import os
 
 flag = True
 while flag:
@@ -21,15 +22,24 @@ while flag:
     # with open('pokemon_height.txt', 'a') as fiw:
     #     fiw.write('Num\tName\tHeight(cm)' + '\n')
 
-    for num in range(1, 152, 1):
+    for num in range(1, 10, 1):
         print('.', end='') # not needed in the code but added to look at the speed of each request
         url = f'https://pokeapi.co/api/v2/pokemon/{num}'
         r = requests.get(url)
         pokemon = r.json()
         name = pokemon['forms'][0]['name']
         height_cm = pokemon["height"]*10  # data in decimeters
-        with open('pokemon_height.txt', 'a') as fiw:
-            fiw.write(f'{num}\t{name.capitalize()}\t{height_cm}' + '\n')
+        # with open('pokemon_height.txt', 'a') as fiw:
+        #     fiw.write(f'{num}\t{name.capitalize()}\t{height_cm}' + '\n')
+
+        url_image = f'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/{num}.png'
+        image_object = requests.get(url_image, allow_redirects=True)
+        pathname = 'images/pokemon_images'
+        os.path.dirname(pathname)
+        with open(f'{num}_{name}_image.png', 'wb') as fiw:
+            fiw.write(image_object)
+
+
 
     flag = False
     print()
